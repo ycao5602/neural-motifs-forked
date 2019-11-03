bf3429bbc05ff3784696017ac839b5a4914f9ad3
ycao5602 <ycao5602@uni.sydney.edu.au>
2019-11-03 16:28:02 +1100

    git push

--- dataloaders/visual_genome.py
+++ dataloaders/visual_genome.py
@@ -93,13 +93,13 @@ class VG(Dataset):
         #         Hue(),
         #     ]))
 
-        # tform = [
-        #     SquarePad(),
-        #     Resize(IM_SCALE),
-        #     ToTensor(),
-        #     Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        # ]
-        # self.transform_pipeline = Compose(tform)
+        tform = [
+            SquarePad(),
+            Resize(IM_SCALE),
+            ToTensor(),
+            Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+        self.transform_pipeline = Compose(tform)
 
     @property
     def coco(self):
