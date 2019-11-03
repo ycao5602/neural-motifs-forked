d9a9b89af96dfda80061ca903aace4bcb19ad20b
ycao5602 <ycao5602@uni.sydney.edu.au>
2019-11-04 10:28:35 +1100

    dev

--- dataloaders/visual_genome.py
+++ dataloaders/visual_genome.py
@@ -49,7 +49,7 @@ class VG(Dataset):
         self.roidb_file = roidb_file
         self.dict_file = dict_file
         # if mode=='val': mode ='dev'
-        self.split='val'
+        self.split='dev'
         image_file = '/share/yutong/projects/bottom-up-attention/data/coco/'+self.split+'.txt'
         self.filter_non_overlap = filter_non_overlap
         self.filter_duplicate_rels = filter_duplicate_rels and self.mode == 'train'
